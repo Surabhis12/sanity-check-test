67f77adc06403fd2f0aef0b4855bf9bd249c976a
Surabhis12 <surabhigowda9986@gmail.com>
2025-11-22 11:42:29 +0530

    updated good code

--- src/test.dart
+++ src/test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class Calculator {
   int add(int a, int b) => a + b;
 }
@@ -5,7 +7,5 @@ class Calculator {
 void main() {
   final calc = Calculator();
   final result = calc.add(10, 20);
-  // debugPrint is allowed; print() is not
-  // ignore: avoid_print
-  print(result);
+  debugPrint('Result: $result'); // Allowed
 }

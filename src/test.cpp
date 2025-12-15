e71e5b60ff469e5cb62268b06ea6861995bda09b
Surabhis12 <surabhigowda9986@gmail.com>
2025-12-15 12:29:59 +0530

    Add PR test file

--- src/test.cpp
+++ src/test.cpp
@@ -6,12 +6,8 @@
 class Calculator {
 private:
     double result;
-
-
 public:
     Calculator() : result(0.0) {}
-
-
     double add(double a, double b) {
         result = a + b;
         return result;

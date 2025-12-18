efa5af320c42f56311666dbfa9520a62deb58697
Surabhis12 <surabhigowda9986@gmail.com>
2025-12-18 12:59:07 +0530

    fix: update actions to v4, remove timeout-minute

--- src/test.java
+++ src/test.java
@@ -1,9 +1,19 @@
-import java.util.*;
-   
+import java.util.logging.Logger;
 
-   public class Test {
-       public static void main(String[] args) {
-           System.out.println("test");
-           String password = "hardcoded123";
-       }
-   }
+public class Test {
+
+    private static final Logger LOGGER = Logger.getLogger(Test.class.getName());
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Application started");
+
+        String userName = System.getenv("APP_USER");
+
+        if (userName == null || userName.isBlank()) {
+            LOGGER.warning("APP_USER environment variable not set");
+        } else {
+            LOGGER.info("Welcome " + userName);
+        }
+    }
+}
